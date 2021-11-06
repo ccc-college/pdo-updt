a49f0011e0ad08214e45785a8ea0a870c831e092
ccc-goto <42729329+ccc-goto@users.noreply.github.com>
2021-11-06 16:15:39 +0900

    1105

--- list_u.php
+++ list_u.php
@@ -4,7 +4,7 @@ require_once("functions.php");
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     if(isset($_POST["name"])){
-        if(! empty($_POST["name"])) }
+        if(! empty($_POST["name"])) {
            $name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
            $_SESSION["name"] = $_POST["name"];
         }
